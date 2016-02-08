25e77d3c985e81d78638da2ecc8d7fc20dc0dbeb
jmangel <john.m5321@yahoo.com>
2016-02-07 22:04:17 -0800

    Links Gameplay scene to Play button

---
+++ IV_Run/Assets/Scripts/PlayButtonScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayButtonScript : MonoBehaviour {
+	
+	public void onClick(){
+		SceneManager.LoadScene (1);
+	}
+}
