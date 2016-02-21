3cfd24fa2b073faceccae40868728f6bf9d7cd5b
Nicholas Frey <FrostyNarwhal@ResNet-25-226.resnet.ucsb.edu>
2016-02-21 13:00:25 -0800

    Added building spawn script
    yeah

---
+++ IV_Run/Assets/Scripts/BuildingSpawnScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingSpawnScript : MonoBehaviour {
+
+	public GameObject[] obj;
+	public float spawnMin = .5f;
+	public float spawnMax = 1f;
+
+	//array of possible obstacle positions
+	private int[] ranges = new int[2] {-5,-27};
+
+	// Use this for initialization
+	void Start ()
+	{
+		Spawn ();
+	}
+
+	void Spawn()
+	{
+		Instantiate(obj[Random.Range(0,obj.GetLength(0))], new Vector3(ranges[Random.Range(0, ranges.Length)], transform.position.y, transform.position.z), Quaternion.identity);
+		Invoke ("Spawn",Random.Range(spawnMin, spawnMax));
+	}
+
+}
