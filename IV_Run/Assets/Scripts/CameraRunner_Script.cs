4a1bf7094d7e7e8471c5f606f459438a25473160
stlenoski <StevensMac@Stevens-MacBook-Pro.local>
2016-02-20 19:42:34 -0800

    more little things
    destructors now work again

---
+++ IV_Run/Assets/Scripts/CameraRunner_Script.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRunner_Script : MonoBehaviour {
+
+	public Transform player;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = new Vector3 (-15, 70, player.position.z - 75);
+	}
+}
--- IV_Run/Assets/Scripts/Destroyer_Script.cs
+++ IV_Run/Assets/Scripts/Destroyer_Script.cs
@@ -6,7 +6,7 @@ public class Destroyer_Script : MonoBehaviour {
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.transform.parent) {
-			Destroy (other.gameObject.transform.parent.gameObject);
+			Destroy(other.gameObject.transform.parent.gameObject);
 		} 
 		else {
 			Destroy(other.gameObject);
