fc23dfddae0e9f8ed8fe725d0ae4c272d584ef68
stlenoski <StevensMac@Stevens-MacBook-Pro.local>
2016-02-20 21:02:11 -0800

    plane box colliders got messed up
    fixed

--- IV_Run/Assets/Scripts/Pause_Script.cs
+++ IV_Run/Assets/Scripts/Pause_Script.cs
@@ -1,18 +1,1 @@
-﻿using UnityEditor;
-using UnityEngine;
-using System.Collections;
-
-// Shows the Assets menu when you right click on the contextRect Rectangle.
-public class EditorUtilityDisplayPopupMenu : MonoBehaviour {
-	public void OnGUI() {
-		Event evt = Event.current;
-		Rect contextRect = new Rect(10, 10, 100, 100);
-		if (evt.type == EventType.ContextClick) {
-			Vector2 mousePos = evt.mousePosition;
-			if (contextRect.Contains(mousePos)) {
-				EditorUtility.DisplayPopupMenu(new Rect(mousePos.x, mousePos.y, 0, 0), "Assets/", null);
-				evt.Use();
-			}
-		}
-	}
-}
+﻿
