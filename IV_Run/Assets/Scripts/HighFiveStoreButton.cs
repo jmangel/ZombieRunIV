d337cacde141fb778bd7b6ede778c7b2d223f60b
jmangel <john.m5321@yahoo.com>
2016-02-21 13:45:36 -0800

    adds basic HighFiveStore scene

---
+++ IV_Run/Assets/Scripts/HighFiveStoreButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class HighFiveStoreButton : MonoBehaviour
+{
+
+    public void onClick()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ IV_Run/Assets/Scripts/StoreScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoreScript : MonoBehaviour {
+    int duration = 10; //make this get the duration from the database
+    int cost = 20;
+
+    public void onClick()
+    {
+        OnGUI();
+    }
+    void OnGUI()
+    {
+        GUI.Label(new Rect(400, 400, 200, 30), "Current powerup duration: " + duration);
+        GUI.Label(new Rect(400, 430, 300, 30), "Next upgrade duration: " + (duration+5) + ", Cost: " + cost + " high fives");
+   
+    }
+}
