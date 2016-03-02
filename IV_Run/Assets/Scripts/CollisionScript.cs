ad2f237dacb904a9cab3380f9a8430466586bd7f
jmangel <john.m5321@yahoo.com>
2016-03-02 15:19:11 -0800

    playbutton and collisions

--- IV_Run/Assets/Scripts/CollisionScript.cs
+++ IV_Run/Assets/Scripts/CollisionScript.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class CollisionScript : MonoBehaviour
 {
     //initialize variables for invincibility powerups
     public int invincibilityTime = 10; //get this from upgrade settings in database
     public float invincibilityExpire = 0;
+
+    public float recentlyHitExpire = 0;
+    public int recentlyHitTime = 5;
     
     void OnCollisionEnter(Collision collision)
     {
@@ -13,17 +17,26 @@ public class CollisionScript : MonoBehaviour
         {
             invincibilityExpire = Time.time + invincibilityTime;
         }
-        if (collision.gameObject.tag == "Obstacle")
+        
+        if (collision.gameObject.tag == "Untagged")
         {
             if (Time.time > invincibilityExpire)
             {
                 //do obstacular stuffs
+                if (Time.time > recentlyHitExpire) //if you haven't been hit recently
+                {
+                    Destroy(collision.gameObject);
+                    recentlyHitExpire = Time.time + recentlyHitTime;
+                }
+                else //if this is second hit in short amount of time
+                {
+                    SceneManager.LoadScene(0);
+                }
             }
-        }
-        if (collision.gameObject.tag == "Untagged")
-        {
-            //GameObject.Find("Character").GetComponent<Controller_Script>().showGUI = true;
-            Time.timeScale = 0;
+            else //if invincible
+            {
+                Destroy(collision.gameObject);
+            }
         }
     }
 }
