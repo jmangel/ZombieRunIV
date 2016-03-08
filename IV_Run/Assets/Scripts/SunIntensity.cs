b8788835f97d7a003a078d334962537dc01383ba
Nicholas Frey <FrostyNarwhal@169-231-87-193.wireless.ucsb.edu>
2016-03-08 15:51:44 -0800

    Added sunlight gradient
    Based on sun’s Y-posiiton

---
+++ IV_Run/Assets/Scripts/SunIntensity.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunIntensity : MonoBehaviour {
+	public float duration = 1.0F;
+    public Light lt;
+    void Start() {
+        lt = GetComponent<Light>();
+    }
+    void Update() {
+		float pos = GameObject.Find("Sun").transform.position.y;
+		pos = (pos + 28)/120;
+        lt.intensity = pos;
+    }
+}
