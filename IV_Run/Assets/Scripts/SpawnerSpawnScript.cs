e66c95d0603d475faf7cf369afd90e9eb0be0ee6
Nicholas Frey <FrostyNarwhal@ResNet-24-98.resnet.ucsb.edu>
2016-03-08 19:34:40 -0800

    Fixed sprite lighting

---
+++ IV_Run/Assets/Scripts/SpawnerSpawnScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerSpawnScript : MonoBehaviour {
+	public GameObject[] obj;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (GameObject.Find ("Sun").transform.position.y < -60) {
+		Instantiate(
+		}
+	}
+}
